e998abe6fb003ad60e4add61ecd7d06c57231694
le tuan dat <letuandattrumrules@gmail.com>
2023-10-07 23:25:07 +0700

    w176

--- OOP/buoi_1/Giai_PTB2.java
+++ OOP/buoi_1/Giai_PTB2.java
@@ -4,11 +4,7 @@ import java.util.Scanner;
 
 public class Giai_PTB2 {
 	public static Scanner sc = new Scanner(System.in);
-	
-	//ax^2 + b^x + c = 0;
-	
-	
-	//a = 0
+
 	public static void giaiPTB1(double b, double c) {
 		if(b == 0) {
 			if(c == 0) {
@@ -21,18 +17,19 @@ public class Giai_PTB2 {
 		}
 	}
 	
-	//a khac 0
 	public static void giaiPTB2(double a, double b, double c) {
 		if(a == 0) {
 			giaiPTB1(b, c);
 		} else {
 			double delta = b * b - 4 * a *c ;
 			if(delta < 0) {
-				//ptvn
+				System.out.println("Phương trình vô nghiệm");
 			} else if(delta == 0) {
-				
+				System.out.println("Phương trình có 2 nghiệm là: " + (-b / (2 * a)));
 			} else {
 				double x1 = (-b + Math.sqrt(delta)) / (2 * a);
+				double x2 = (-b - Math.sqrt(delta)) / (2 * a);
+				System.out.println("Phương trình có 2 nghiệm là: " + x1 + ", " + x2);
 			}
 		}
 	}
